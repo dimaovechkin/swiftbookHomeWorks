2051f02ad6b9b79abcf674499899ce3301fbc9ab
Dmitri Ovechkin <58566562+dimaovechkin@users.noreply.github.com>
2023-08-02 22:47:55 +0300

    hw2.1

---
+++ hw2.1/hw2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2.1
+//
+//  Created by Dmitri Ovechkin on 02.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
