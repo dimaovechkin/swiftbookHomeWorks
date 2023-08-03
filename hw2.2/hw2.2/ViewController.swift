e03627f0801cf0c930283125c58e5cb4f963f55b
Dmitri Ovechkin <58566562+dimaovechkin@users.noreply.github.com>
2023-08-03 17:45:18 +0300

    2.2

--- hw2.2/hw2.2/ViewController.swift
+++ hw2.2/hw2.2/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-enum CurrentLirht {
+enum CurrentLight {
     case green, yellow, red
     
 }
@@ -17,11 +17,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var thirdColour: UIView!
     @IBOutlet weak var button: UIButton!
     
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        button.layer.cornerRadius = 10
+        
+        firstColour.alpha = lightIsOff
+        secondColour.alpha = lightIsOff
+        thirdColour.alpha = lightIsOff
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        firstColour.layer.cornerRadius = firstColour.frame.width / 2
+        secondColour.layer.cornerRadius = firstColour.frame.width / 2
+        thirdColour.layer.cornerRadius = firstColour.frame.width / 2
     }
 
-
+    @IBAction func startButtonPressed(_ sender: Any) {
+        
+        button.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .green:
+            firstColour.alpha = lightIsOn
+            secondColour.alpha = lightIsOff
+            currentLight = .red
+        case .yellow:
+            firstColour.alpha = lightIsOff
+            secondColour.alpha = lightIsOn
+            currentLight = .green
+        case .red:
+            firstColour.alpha = lightIsOn
+            secondColour.alpha = lightIsOff
+            currentLight = .yellow
+        }
+    }
+    
+    
+    
 }
 
